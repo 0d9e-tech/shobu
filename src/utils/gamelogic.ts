5d08785dfb91ef1661b41bca875361d4cce7d15c
Matuush <m4tuush@gmail.com>
2024-05-27 17:06:05 +0200

    zakládání hry

---
+++ src/utils/gamelogic.ts
@@ -0,0 +1,9 @@
+const createGame = () => {
+   let state:number[] = [];
+   for(let i = 0; i < 64; i++) {
+      if((i % 16) / 4 == 0) state.push(1);
+      else if((i % 16) / 4 == 3) state.push(2);
+      else state.push(0);
+   }
+   return state;
+}
