7f7be664df68580c00023f2ac53e9367ee49cad5
Matuush <m4tuush@gmail.com>
2024-05-27 17:21:10 +0200

    I am dying

--- src/utils/gamelogic.ts
+++ src/utils/gamelogic.ts
@@ -2,8 +2,9 @@ const createGame = () => {
    let state:number[] = [];
    for(let i = 0; i < 64; i++) {
       if(Math.floor((i % 16) / 4) == 0) state.push(1);
-      else if(Math.floor((i % 16) / 4 == 3)) state.push(2);
+      else if(Math.floor((i % 16) / 4) == 3) state.push(2);
       else state.push(0);
    }
    return state;
 }
+
