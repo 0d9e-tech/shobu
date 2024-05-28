3728aac97f7b2b9b0bd94020f7737be1d08e1c9a
Matěj Volf <git@mvolfik.com>
2024-05-28 18:09:44 +0200

    add keys to forloop children

--- src/app/_components/shobu-board.tsx
+++ src/app/_components/shobu-board.tsx
@@ -41,9 +41,9 @@ function SubBoard(props: { sub: number[][], z: number, clicked: (vec: {}) => voi
     return (
         <div className="flex flex-col gap-2 lg:gap-3">
             {[0, 1, 2, 3].map(y => (
-                <div className="flex flex-row gap-2 lg:gap-3">
+                <div key={y} className="flex flex-row gap-2 lg:gap-3">
                     {[0, 1, 2, 3].map(x => (
-                        <div onClick={() => clicked([x, y, z])} className="w-11 h-11 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 border-2 rounded sm:rounded-md md:rounded-xl bg-stone-300 flex">
+                        <div key={x} onClick={() => clicked([x, y, z])} className="w-11 h-11 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 border-2 rounded sm:rounded-md md:rounded-xl bg-stone-300 flex">
                             { sub[y]![x] != 0 &&
                                 <div className={`"w-9 h-9 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-18 lg:h-18 rounded-full m-auto ${sub[y]![x] == 1 ? "bg-stone-600" : "bg-stone-900"}`} />
                             }
